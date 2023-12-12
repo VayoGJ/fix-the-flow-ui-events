daf15fe56f8a220408e5808181ae58781260afec
VayoGJ <144008258+VayoGJ@users.noreply.github.com>
2023-12-12 17:05:01 +0100

    werkt

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,12 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+let color = document.querySelector(`a:nth-of-type(1)`)
+
+color.addEventListener(`click`, colorchange)
+color.addEventListener(`animationend`, colorchange)
+
+function colorchange(){
+  color.classList.toggle(`colorchange`)
+}
